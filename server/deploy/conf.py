0c8904d2bb1a6e476e24e5a1b0a25a43220094dc
redy <redy.ru@gmail.com>
2017-10-23 10:05:29 +0800

    xxx

--- server/deploy/conf.py
+++ server/deploy/conf.py
@@ -1,10 +1,4 @@
 # coding=utf-8
 from __future__ import absolute_import
 
-import os
-
-
 BASE_DEPLOY_DIR = '/data/deployment_data/julolo'
-
-if not os.path.isdir(BASE_DEPLOY_DIR):
-    os.makedirs(BASE_DEPLOY_DIR)
