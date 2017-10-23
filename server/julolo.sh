866ca8c6fef86214118292bab16c520f610c963d
redy <redy.ru@gmail.com>
2017-10-23 11:12:29 +0800

    xxx

--- server/julolo.sh
+++ server/julolo.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+mkdir -p /data/deployment_data/julolo/log
+mkdir -p /data/deployment_data/julolo/tmp
+
+cd /var/www/julolo/ && git stash save --keep-index && git stash drop
+cd /var/www/julolo/ && find . -name \*.pyc -delete
+cd /var/www/julolo/ && git pull origin master
+
 for arg in $@
 do
-  if [ $arg = "-p" -o $arg = "--packages" ]
+  if [ $arg = "-r" -o $arg = "--refresh" ]
     then
-      echo 'Setup Peon with reuiqred packages'
-      sudo pip install -r requirements.txt
-      sudo npm install -g coffeescript less uglify-js
+      echo 'Refresh ...'
+      cd /var/www/julolo/server/ &&
+      python toolkits.py migration -p &&
+      python toolkits.py index -p
   fi
 done
 
-sudo python setup.py install
+sudo supervisorctl restart julolo
+sudo supervisorctl restart julolo_master
