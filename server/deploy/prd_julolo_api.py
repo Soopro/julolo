e0fb8cf30eefecee8df7b12f3b018db7d3ae8c58
redy <redy.ru@gmail.com>
2017-10-23 10:59:36 +0800

    xxx

--- server/deploy/prd_julolo_api.py
+++ server/deploy/prd_julolo_api.py
@@ -2,15 +2,12 @@
 from __future__ import absolute_import
 import multiprocessing
 
-from config import config
 
-
-conf_name = 'production'
-cfg = config.get(conf_name)
+DEPLOY_DIR = '/data/deployment_data/julolo'
 
 bind = '127.0.0.1:15800'
 workers = multiprocessing.cpu_count() * 2 + 1
-accesslog = '{}/log/julolo_api.access.log'.format(cfg.LOG_FOLDER)
-errorlog = '{}/log/julolo_api.error.log'.format(cfg.LOG_FOLDER)
-pidfile = '{}/julolo_api.pid'.format(cfg.DEPLOY_DIR)
-raw_env = 'JULOLO_CONFIG_NAME={}'.format(conf_name)
+accesslog = '{}/log/julolo_api.access.log'.format(DEPLOY_DIR)
+errorlog = '{}/log/julolo_api.error.log'.format(DEPLOY_DIR)
+pidfile = '{}/julolo_api.pid'.format(DEPLOY_DIR)
+raw_env = 'JULOLO_CONFIG_NAME=production'
--- server/deploy/test_julolo_admin.py
+++ server/deploy/test_julolo_admin.py
@@ -2,15 +2,12 @@
 from __future__ import absolute_import
 import multiprocessing
 
-from config import config
 
-
-conf_name = 'production'
-cfg = config.get(conf_name)
+DEPLOY_DIR = '/data/deployment_data/julolo'
 
 bind = '127.0.0.1:15809'
 workers = min(multiprocessing.cpu_count(), 2)
-accesslog = '{}/log/julolo_admin.access.log'.format(cfg.LOG_FOLDER)
-errorlog = '{}/log/julolo_admin.error.log'.format(cfg.LOG_FOLDER)
-pidfile = '{}/julolo_admin.pid'.format(cfg.DEPLOY_DIR)
-raw_env = 'JULOLO_CONFIG_NAME={}'.format(conf_name)
+accesslog = '{}/log/julolo_admin.access.log'.format(DEPLOY_DIR)
+errorlog = '{}/log/julolo_admin.error.log'.format(DEPLOY_DIR)
+pidfile = '{}/julolo_admin.pid'.format(DEPLOY_DIR)
+raw_env = 'JULOLO_CONFIG_NAME=testing'
