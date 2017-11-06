a719eb428d033af7a16261d75621782ae187d8f4
redy <redy.ru@gmail.com>
2017-11-06 22:45:10 +0800

    xxx

--- server/common_models/__init__.py
+++ server/common_models/__init__.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import
 from .user import *
 from .category import *
 from .promotion import *
-from .event import *
 from .media import *
 from .tip import *
 from .store import *
